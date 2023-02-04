53c3396f0103a140e543e66e45ec48d4445f5265
alvesdesouzaalex <alvesdesouzaalex@gmail.com>
2023-02-04 07:38:10 -0300

    add: first commit

---
+++ src/main/java/br/com/mb/moviesbattleapp/MoviesBattleAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.mb.moviesbattleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesBattleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesBattleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mb/moviesbattleapp/MoviesBattleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mb.moviesbattleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class MoviesBattleAppApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
